ba105347cec9d46e68c712599c0af005086b9f15
ipotatochip <akhilpati3@gmail.com>
2022-08-01 12:03:24 +0530

    home

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*********************/
+/*  01. Pre-Loader   */
+/*********************/
+
+// $(window).on('load',function() {
+//     $('#preloader_run').fadeOut();
+//     $('#preloader').delay(350).fadeOut();
+// });
+
+
+// $(window).on( "load",function() {
+// 	$("#preloader_run").fadeOut("slow");
+//   $("#preloader").delay(2000).fadeOut("slow");
+// });
+
+$(window).load(function() {
+    $('.preloader').fadeOut('slow');
+ });
