8b6aee50b920692cceba6510c9a4d5cdedd4a374
ipotatochip <akhilpati3@gmail.com>
2022-08-27 17:38:52 +0530

    cards_apdate

--- js/script.js
+++ js/script.js
@@ -2,17 +2,8 @@
 /*  01. Pre-Loader   */
 /*********************/
 
-// $(window).on('load',function() {
-//     $('#preloader_run').fadeOut();
-//     $('#preloader').delay(350).fadeOut();
-// });
 
-
-// $(window).on( "load",function() {
-// 	$("#preloader_run").fadeOut("slow");
-//   $("#preloader").delay(2000).fadeOut("slow");
-// });
-
-$(window).load(function() {
-    $('.preloader').fadeOut('slow');
- });
+$(window).on('load',function() {
+    $('#preloader_run').fadeOut();
+    $('#preloader').delay(350).fadeOut();
+});
